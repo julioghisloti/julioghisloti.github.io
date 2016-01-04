bc96091c2d92746de3dc8a901905e6d09626346c
Júlio César <julio@MacBook-Pro-de-Julio.local>
2016-01-04 21:02:44 -0200

    starting new layout, jobs page, sigle page plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
-var gulp      = require('gulp');
-var gutil     = require('gulp-util');
-var stylus    = require('gulp-stylus');
-var nib       = require('nib');
-var jeet      = require('jeet');
-var uglify    = require('gulp-uglify');
-var plumber   = require('gulp-plumber');
-var watch     = require('gulp-watch');
+var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var stylus      = require('gulp-stylus');
+var nib         = require('nib');
+var jeet        = require('jeet');
+var uglify      = require('gulp-uglify');
+var plumber     = require('gulp-plumber');
+var pngquant    = require('imagemin-pngquant');
+var watch       = require('gulp-watch');
+var concat      = require('gulp-concat');
+var browserSync = require('browser-sync').create();
 
 //compass error
 var onError = function (err) {  
@@ -13,12 +16,27 @@ var onError = function (err) {
   console.log(err);
 };
 
+// Static Server + watching stylus/html files
+gulp.task('serve', ['stylus'], function() {
+
+    browserSync.init({
+       proxy: "localhost:1337/julioghisloti.github.io"
+    });
+
+    gulp.watch("src/styl/**/*.styl", ['stylus']);
+    gulp.watch("src/js/**/*.js", ['scripts']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+
+});
+
+gulp.task('default', ['serve']);
+
 //minificando js
 gulp.task('scripts', function(){
-    return gulp  
-        .src(['src/js/*.js'])
+    return gulp.src(['src/js/main.js'])
         .pipe(uglify())
-        .pipe(gulp.dest('build/js/'));
+        .pipe(gulp.dest('build/js/'))
+        .pipe(browserSync.stream());
 });
 
 //stylus
@@ -28,25 +46,6 @@ gulp.task('stylus', function(){
             use: [nib(), jeet()],
             compress: true
         }))
-        .pipe(gulp.dest('build/css/'));
-});
-
-
-//watch
-gulp.task('watch', function(){
-
-
-    //Scripts
-    gulp.watch('src/js/**/*.js', function(event) {
-        gutil.log('Minificando seu JS ......');
-        gulp.run('scripts');
-    });
-
-    //Compass
-    gulp.watch('src/styl/**/*.styl', function(event) {
-        gutil.log('Compilando o seu Stylus ......');
-        gulp.run('stylus');
-    });
-
-
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.stream());
 });
