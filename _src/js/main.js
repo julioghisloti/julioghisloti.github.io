ac8e5f9febb4274a9f0328e68006c68447240aca
Julio <criacao1@mobme.com.br>
2020-05-11 15:58:43 -0300

    atualizando menu

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,9 +1,46 @@
-$(function(){
-    $('.menu').on('click', function(event){
-        event.preventDefault();
+function fadeInPage(){
+	if (!window.AnimationEvent) { return; }
+	var fader = document.getElementById('fader');
+	fader.classList.add('fade-out');
+}
 
-        $('.menu-over').toggleClass('m-o-a');
-    });
+//parent.appendChild(el).classList.add('full');;
 
-    $("p").find('img').parent().addClass("full");
+document.addEventListener('DOMContentLoaded', function() {
+	if (!window.AnimationEvent) { return; }
+	var anchors = document.getElementsByTagName('a');
+	for (var idx=0; idx<anchors.length; idx+=1) {
+		if (anchors[idx].hostname !== window.location.hostname ||
+	        anchors[idx].pathname === window.location.pathname) {
+	        continue;
+	    }
+
+	    anchors[idx].addEventListener('click', function(event) {
+            var fader = document.getElementById('fader'),
+                anchor = event.currentTarget;
+            
+            var listener = function() {
+                window.location = anchor.href;
+                fader.removeEventListener('animationend', listener);
+            };
+            fader.addEventListener('animationend', listener);
+            
+            event.preventDefault();
+
+            fader.classList.add('fade-in');
+        });
+    }
+
+  var img = document.getElementsByTagName("img");
+  for (var i = 0; i < img.length; i+=1){
+    img[i].parentElement.classList.add('full');
+  }
+});
+
+window.addEventListener('pageshow', function (event) {
+  if (!event.persisted) {
+    return;
+  }
+  var fader = document.getElementById('fader');
+  fader.classList.remove('fade-in');
 });
